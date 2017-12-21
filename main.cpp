8403f0bf247bc643b8fadb0cad71695db7fdbe63
nextsigner <nextsigner@gmail.com>
2017-12-21 12:11:03 -0300

    he eliminado el qrc

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     qmlRegisterType<UK>("uk", 1, 0, "UK");
-    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+    engine.load(QUrl(QLatin1String("main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
 
